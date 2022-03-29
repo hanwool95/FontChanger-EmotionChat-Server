51eb205a35a1f532acdcfe0bdf9fb01cdd51ee3b
hanwool95 <77750865+hanwool95@users.noreply.github.com>
2022-03-30 04:58:12 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# 감정표현 채팅 인터페이스 서버
+
+제작기간: 2021.03 ~ 진행중
+구성원: hanwool95
+문의: hanwool95@snu.ac.kr
+
+스택: Express.js
+
+<a href="https://github.com/hanwool95/FontChanger-EmotionChat-Client">Client part github link(React)</a>
+
+실행방법
+
+~~~
+npm install package.json
+npm start
+~~~
+
---
+++ Server.js
@@ -0,0 +1,31 @@
+
+const express = require("express");
+const http = require('http');
+const PORT = 3737;
+const app = express();
+const cors = require('cors')
+
+
+let server = http.createServer(app);
+server.listen(PORT, () => {
+    console.log(`listening at http://127.0.0.1:${PORT}...`);
+});
+
+let socket = require('socket.io')(server,{
+    cors: {
+        origin : "*",
+        credentials: true
+    }
+});
+
+
+socket.sockets.on('connection', function(client){
+    console.log('Connection!');
+
+    client.on('serverReceiver', ({name, message, emotion}) => {
+        console.log("message recieved")
+        console.log(message)
+        socket.sockets.emit('clientReceiver', ({name, message, emotion}))
+    })
+})
+
