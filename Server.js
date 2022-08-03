a9de9aa9e271e72d0d0b2179715243896e521263
hanwool95 <77750865+hanwool95@users.noreply.github.com>
2022-08-03 20:54:45 +0900

    Delete: Emotion Element

--- Server.js
+++ Server.js
@@ -22,10 +22,10 @@ let socket = require('socket.io')(server,{
 socket.sockets.on('connection', function(client){
     console.log('Connection!');
 
-    client.on('serverReceiver', ({name, message, emotion, font}) => {
+    client.on('serverReceiver', ({name, message, font}) => {
         console.log("message recieved")
         console.log(message)
-        socket.sockets.emit('clientReceiver', ({name, message, emotion, font}))
+        socket.sockets.emit('clientReceiver', ({name, message, font}))
     })
 })
 
